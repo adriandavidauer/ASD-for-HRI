0fb2bfbfed1090af6b591d29a649543b9ed92053
Adrian Auer <adrian.auer@dfki.de>
2026-07-02 12:44:25 +0000

    adding LR-ASD to testable models

--- docker/run_experiments_gpu.bash
+++ docker/run_experiments_gpu.bash
@@ -27,5 +27,7 @@ docker run --gpus all --name ${MODEL} -v $HOME/ASD-for-HRI/data:/app/data asd4hr
 # docker run --gpus all --name ${MODEL} -v $HOME/ASD-for-HRI/data:/app/data asd4hri --data_dir /app/data --no_download --predictions_dir "predictions_${MODEL}" --architecture $MODEL 
 
 export MODEL=TalkNet
-
 docker run --gpus all -v "/Data/data:/app/data" --entrypoint "python" --name "${MODEL}" asd4hri src/TalkNet-ASD/unitalk_on_TalkNet.py --videoFolder /app/data/videos/val --predictions_dir "/app/data/predictions_${MODEL}"
+
+export MODEL=LR-ASD
+docker run --gpus all -v "/Data/data:/app/data" --entrypoint "python" --name "${MODEL}" asd4hri src/LR-ASD/unitalk_on_LRASD.py --videoFolder /app/data/videos/val --predictions_dir "/app/data/predictions_${MODEL}"
