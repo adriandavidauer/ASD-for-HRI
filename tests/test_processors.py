8f7c83cdb7c554854e9068a312320231550e2b37
Adrian Auer <adrian.auer@dfki.de>
2026-07-10 12:56:22 +0200

    started tests and crashed devcontainer

--- tests/test_processors.py
+++ tests/test_processors.py
@@ -6,13 +6,15 @@ All paz processors needed for the full pipeline
 
 
 # 3rd party imports
-
+import numpy as np
 
 # local imports
 from asd4hri.processors import BufferFeatures
 
 # end file header
 __author__      = 'Adrian Auer'
+INPUT_SHAPE=(38, 96, 96, 3)
+test_data = np.empty(INPUT_SHAPE)
 
 def test_BufferFeatures():
-    buffer = BufferFeatures()
+    buffer = BufferFeatures(input_size=INPUT_SHAPE)
